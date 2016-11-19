db10e380d7674f2dff22aef4cc0b8604d041010f
techgaun <coolsamar207@gmail.com>
2016-11-19 01:53:02 -0600

    fix github link and remove assertion

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExErlstats.Mixfile do
 
   def project do
     [app: :ex_erlstats,
-     version: "0.1.1",
+     version: "0.1.2",
      name: "ExErlstats",
      description: "A simple module to get erlang VM stats",
      source_url: "https://github.com/techgaun/ex_erlstats",
@@ -34,8 +34,8 @@ defmodule ExErlstats.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.12", only: :dev},
-
+      {:ex_doc, "~> 0.13", only: :dev},
+      {:earmark, "~> 1.0", only: :dev}
     ]
   end
 
@@ -45,7 +45,7 @@ defmodule ExErlstats.Mixfile do
         "Samar Acharya"
       ],
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/Brightergy/ex_gecko"},
+      links: %{"GitHub" => "https://github.com/techgaun/ex_erlstats", "Website" => "http://samar.techgaun.com"},
       files: ~w(config lib mix.exs README.md LICENSE)
     ]
   end
